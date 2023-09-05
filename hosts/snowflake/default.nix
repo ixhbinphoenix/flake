906283f90090c29b3ee25969534bdecf8207d9f8
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2023-09-05 12:33:58 +0200

    Add ntfs support

--- hosts/snowflake/default.nix
+++ hosts/snowflake/default.nix
@@ -21,6 +21,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.supportedFilesystems = [ "ntfs" ];
+
   networking.hostName = "snowflake";
 
   hardware = {
