78e28a68e3b1786aee888e7179f9726a7f74656a
ixhbinphoenix <phoenix@ixhby.dev>
2024-09-20 19:27:42 +0200

    Add battery status to waybar

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -1,20 +1,62 @@
-{ lib, config, pkgs, ...}:
+{ lib, config, pkgs, ...}: with lib;
 {
   imports = [];
+
   options = {};
 
   config = {
-    home.file.".config/waybar/config" = {
-      source = ./config;
-      recursive = true;
+    # Need to wait for Waybar#3551
+    programs.waybar = {
+      enable = true;
+      settings = {
+        main = {
+         layer = "bottom";
+         position = "top";
+         spacing = 5;
+         modules-right = [
+           "tray"
+           "battery"
+           "network"
+           "clock"
+         ];
+  
+         network = {
+           format-wifi = "  {essid}";
+           format-ethernet = "󰈁 {ipaddr}/{cidr}";
+           format-linked = "󰈂 (no ip)";
+           format-disconnected = "󰈂";
+           family = "ipv4";
+           tooltip = false;
+         };
+  
+         clock = {
+           format = "󰥔 {:%H:%M}";
+           tooltip-format = "{:%H:%M %d %B %Y}";
+         };
+  
+         tray = {
+           spacing = 5;
+         };
+       };
     };
-    home.file.".config/waybar/style.css" = {
-      source = ./style.css;
-      recursive = true;
-    };
-
-    home.packages = with pkgs; [
-      waybar
-    ];
+    style = ''
+      @define-color bg #1e1e2e;
+      @define-color text #cdd6f4;
+  
+      * {
+        border: none;
+        border-radius: 0px;
+        min-height: 0;
+        margin: 0.2em 0.3em 0.2em 0.3em;
+      }
+  
+      #waybar {
+        background: @bg;
+        color: @text;
+        font-size: 0.85rem;
+        font-weight: bold;
+       }
+    '';
+     };
   };
 }
