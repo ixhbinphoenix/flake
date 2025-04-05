e900a800d51ee451e3508f07d7d397273d06c238
ixhbinphoenix <phoenix@ixhby.dev>
2025-04-05 02:46:37 +0200

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/stable.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/release-2.92.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur.url = "github:nix-community/NUR";
@@ -48,7 +48,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    conduwuit.url = "https://git.gay/june/conduwuit/archive/main.tar.gz";
+    conduwuit.url = "github:girlbossceo/conduwuit";
 
     quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
     quadlet-nix.inputs.nixpkgs.follows = "nixpkgs";
--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    yubikey-manager-qt
     yubikey-personalization-gui
     yubico-piv-tool
     yubioath-flutter
