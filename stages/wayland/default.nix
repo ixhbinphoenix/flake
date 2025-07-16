a7aa4e59096789655f6d6ca8e94277c315d9a903
ixhbinphoenix <phoenix@ixhby.dev>
2025-07-16 18:28:25 +0200

    fix links

--- stages/wayland/default.nix
+++ stages/wayland/default.nix
@@ -82,6 +82,7 @@ with lib;
         defaultApplications = {
           "text/html" = "librewolf.desktop";
           "x-scheme-handler/http" = "librewolf.desktop";
+          "x-scheme-handler/https" = "librewolf.desktop";
           "x-scheme-handler/about" = "librewolf.desktop";
           "x-scheme-handler/unknown" = "librewolf.desktop";
           "video/mp4" = "mpv.desktop";
